7ebaf13c21603bb0af0fc23d35419d92953a5178
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-28 09:08:00 -0700

    add functionality to new-question form, sending action up to index to save in database

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes')
+      };
+      this.sendAction('save', params);
+      this.set('author', '');
+      this.set('content', '');
+      this.set('notes', '');
+    }
+  }
 });
