852614d4f30f8ebbbd4a217d4be27bd76539e19b
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-28 09:26:25 -0700

    add edit form to update-question component with functionality

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    editQuestion(question) {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes')
+      };
+      this.sendAction('editQuestion', params, question);
+    }
+  }
 });
