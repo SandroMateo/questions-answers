e4ae0dc0d1d5b1be6b3f7611a0dbf1be4f5c49cb
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-27 18:11:19 -0700

    add foreach loop in index.hbs to display content in question-list component

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr('string'),
   author: DS.attr('string'),
-  question: DS.belongTo('question', { async: true })
+  question: DS.belongsTo('question', { async: true })
 });
