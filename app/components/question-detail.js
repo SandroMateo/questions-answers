3488e16f0be0ed5253089ed83e2387cc54fb4767
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-04 10:16:13 -0700

    add computed sortedReviews computed property to question-detail in order to sort reviews from most liked to least liked

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showForm: Ember.inject.service('show-hide'),
+  sortBy: ['likes:desc'],
   answerAmount: Ember.computed('question.answers', function() {
     return this.get('question.answers').content.length;
   }),
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
+
   actions: {
     deleteQuestion(question) {
       this.sendAction('deleteQuestion', question);
