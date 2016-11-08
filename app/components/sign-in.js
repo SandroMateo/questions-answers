a89a34f50d54996e8a2cf3f92281f0b962da76bd
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-07 19:21:20 -0800

    add a form to sign in and sign out in sign-in component

--- app/components/sign-in.js
+++ app/components/sign-in.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  loggedIn: Ember.inject.service(),
+
+  actions: {
+    signIn() {
+      this.get('loggedIn').login(this.get('name'));
+    },
+
+    signOut() {
+      this.get('loggedIn').logout();
+    }
+  }
 });
