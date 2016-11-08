336d3846c799579e1e00cd6ba94a144d1a46e474
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-07 19:08:44 -0800

    add logged-in service, login route, and sign-in component

---
+++ app/services/logged-in.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  signedIn: false,
+  name: '',
+
+  login(name) {
+    this.set('signedIn', true);
+    this.set('name', name);
+  }
+
+  logout() {
+    this.set('signedIn', false);
+    this.set('name', '');
+  }
+});
